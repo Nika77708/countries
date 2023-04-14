e6bb7f9d4d0b16199be732880e19aadb54f4d70c
Nika77708 <nika77708@gmail.com>
2023-04-14 16:58:19 +0200

    Second commit

--- src/js/fetch_countries.js
+++ src/js/fetch_countries.js
@@ -9,7 +9,7 @@
 const SEARCH_URL = 'https://restcountries.com/v3.1/name/';
 
 const searchParams = new URLSearchParams({
-  fields: ['name', 'capital', 'population', 'flag', 'languages'],
+  fields: ['name', 'capital', 'population', 'flags', 'languages'],
 });
 
 export const fetchCountries = name => {
@@ -17,18 +17,7 @@ export const fetchCountries = name => {
     if (!response.ok) {
         throw new Error (response.status);
     }
-    // console.log(response);
      return response.json();
   });
 };
 
-// Фильтрация полей
-// В ответе от бэкенда возвращаются объекты, большая часть свойств которых тебе не пригодится. Чтобы сократить объем передаваемых
-// данных добавь строку параметров запроса - так этот бэкенд реализует фильтрацию полей. Ознакомься с документацией синтаксиса фильтров.
-
-// Тебе нужны только следующие свойства:
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языков
